81ec25fa5e14653ea9d1a8f48d6c03f389620a9a
lomza7 <Louis@maaza.pro>
2025-12-08 20:42:52 +0100

    Updated Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -121,7 +121,7 @@ export default function Navbar() {
 
             <Link
               to="/comparaison"
-              className="px-5 py-2.5 text-sm font-medium text-gray-700 hover:bg-gray-50 rounded-full transition"
+              className="px-5 py-2.5 text-sm font-medium text-gray-700 hover:bg-gray-50 rounded-full transition hidden md:block"
             >
               {language === 'fr' ? 'Comparaison' : 'Comparison'}
             </Link>
